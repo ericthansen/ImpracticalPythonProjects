776cbca6c662bb8ed564c12aa3f1d558c32e5455
Eric H <20998658+ericthansen@users.noreply.github.com>
2020-01-31 21:04:40 -0600

    'pseudoMainChange'

--- pseudonym.py
+++ pseudonym.py
@@ -1,26 +1,29 @@
 import sys, random
+def main():
+	print("Welcome to the 'Sidekick Name Generator.' \n")
+	print("For funny names usable for comedy, improv, or RPG night.")
 
-print("Welcome to the 'Sidekick Name Generator.' \n")
-print("For funny names usable for comedy, improv, or RPG night.")
+	first = ('Applejack', 'AaronBurr', 'Baby Arm', 'Chinstrap', 'Colloidal Silver', 'Chitty Chitty Bang Bang', 'Doghouse', 'Dearth', 'Eustus', 
+		'Frankenfurter', 'Geneologous', 'Harvey', 'Harrison', 'Itchy Itchy Ichabod', 'And Many More'
+		)
 
-first = ('Applejack', 'AaronBurr', 'Baby Arm', 'Chinstrap', 'Colloidal Silver', 'Chitty Chitty Bang Bang', 'Doghouse', 'Dearth', 'Eustus', 
-	'Frankenfurter', 'Geneologous', 'Harvey', 'Harrison', 'Itchy Itchy Ichabod', 'And Many More'
-	)
+	last = ('Aardvaar', 'Bluetooth', 'Chattan', 'Derby', 'Elephantitis', 'Flaxseed', 'Garp', 'Hornsby', 'Ichabod', 'Jingle-Jangle Jones', 'Kakarot', 'Looper',
+		'Merriadoc', 'Nethermoops', 'O.P. Orangutan', 'Pinkerton', 'Quiescent', 'Ronaldo', 'Stroganofshine', 'Tippletink', 'Underlip', 'Viola', 'Whizbang', 'X-ray-Specs Xanth', 'Zoom, PhD'
+		)
 
-last = ('Aardvaar', 'Bluetooth', 'Chattan', 'Derby', 'Elephantitis', 'Flaxseed', 'Garp', 'Hornsby', 'Ichabod', 'Jingle-Jangle Jones', 'Kakarot', 'Looper',
-	'Merriadoc', 'Nethermoops', 'O.P. Orangutan', 'Pinkerton', 'Quiescent', 'Ronaldo', 'Stroganofshine', 'Tippletink', 'Underlip', 'Viola', 'Whizbang', 'X-ray-Specs Xanth', 'Zoom, PhD'
-	)
+	while True:
+		firstName = random.choice(first)
+		lastName = random.choice(last)
 
-while True:
-	firstName = random.choice(first)
-	lastName = random.choice(last)
+		print("/n/n")
+		print("{} {}".format(firstName, lastName), file=sys.stderr)
+		#print("{} {}".format(firstName, lastName))
 
-	print("/n/n")
-	print("{} {}".format(firstName, lastName), file=sys.stderr)
-	#print("{} {}".format(firstName, lastName))
+		try_again = input("\n\n Try again? (Press Enter else n to quit)\n")
+		if try_again.lower()=='n':
+			break
 
-	try_again = input("\n\n Try again? (Press Enter else n to quit)\n")
-	if try_again.lower()=='n':
-		break
+	input('Press Enter to exit.')
 
-input('Press Enter to exit.')
+if __name__ == "__main__":
+	main()
